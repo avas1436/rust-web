b220a904d11c225aa9b4665f4e26cc346c3e22ea
avAs zade <abas.zade@outlook.com>
2026-02-14 18:44:11 +0330

    Add DTO to project.

---
+++ dto/dto_dto.rs
@@ -0,0 +1,11 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct CreateDtoDto {
+    // TODO: define fields
+}
+
+#[derive(Deserialize)]
+pub struct UpdateDtoDto {
+    // TODO: define fields
+}
---
+++ dto/mod.rs
@@ -0,0 +1,3 @@
+// module: dto
+
+pub mod dto_dto;

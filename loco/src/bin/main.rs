bf5ce124ee76a4df647913601339028edabe1783
avAs zade <abas.zade@outlook.com>
2026-02-17 22:40:52 +0330

    remove light project beacause its too small and simple.

--- loco/src/bin/main.rs
+++
@@ -1,7 +0,0 @@
-use loco::app::App;
-use loco_rs::cli;
-
-#[tokio::main]
-async fn main() -> loco_rs::Result<()> {
-    cli::main::<App>().await
-}
--- loco/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod app;
-pub mod controllers;
-pub mod data;
-pub mod initializers;
-pub mod tasks;
-pub mod views;
